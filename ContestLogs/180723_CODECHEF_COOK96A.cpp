bd7dce4b1c5319d38089fcffcb0afff5de349c19
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2018-07-23 13:18:49 +0800

    Create 180723_CODECHEF_COOK96A.cpp

---
+++ ContestLogs/180723_CODECHEF_COOK96A.cpp
@@ -0,0 +1,48 @@
+Contest log for July Cook-Off 2018 Division 1
+Log started on 2018-07-22 23:59:15.243
+00:04:51:5 Submitted "OR Matrix"
+00:05:19:7 WA "OR Matrix", forgot to reset between tests
+00:06:43:4 AC "OR Matrix"
+00:06:58:0 Reading "Chef and GCD Game"
+00:18:32:5 submitting "Chef and GCD Game"
+00:18:49:1 Reading "Finding Second Maximum"
+00:19:13:9 WA "Chef and GCD Game", forgot to reset stuff between tests again
+00:21:36:6 WA "Chef and GCD Game", stupid incorrect bound
+00:22:46:6 AC "Chef and GCD Game"
+00:24:35:3 "Chef and Queries on a Tree" is basically just path queries with HLD and persistent segment tree
+00:30:30:9 Coding "Sticks"
+00:39:35:6 WA "Sticks"
+00:41:54:3 Forgot to subtract from k, resubmitting
+00:42:17:0 WA "Sticks"
+00:45:20:4 Resubmitting, this time subtracting properly
+00:45:45:6 WA "Sticks"
+00:46:41:6 Submitting again
+00:48:03:6 WA "Sticks"
+00:53:03:3 Found WA case
+01:00:41:9 Submitting
+01:01:28:3 WA "Sticks"
+01:05:10:0 Submitting
+01:05:51:2 WA "Sticks"
+01:08:04:2 More edge cases :/
+01:09:25:5 Had to cast size() to int
+01:10:20:6 WA "Sticks"
+01:13:35:5 Can't find bad case but submitting anyways
+01:14:05:8 WA "Sticks"
+01:15:25:1 Found bad case
+01:24:24:0 Submitting
+01:25:12:6 WA "Sticks"
+01:26:21:5 Typo k2->k
+01:26:58:9 WA "Sticks"
+01:29:10:5 WA "Sticks"
+01:31:28:0 im gonna start working on "Finding Second Maximum"
+01:33:33:1 Submitting "Finding Second Maximum"
+01:35:01:8 WA "Finding Second Maximum"
+01:37:23:6 Submitting "Finding Second Maximum" again
+01:38:03:4 WA "Finding Second Maximum"
+01:48:30:8 Submitting "Sticks"
+01:48:34:2 WA "Sticks"
+02:13:57:6 Submitting "Finding Second Maximum"
+02:14:25:4 WA "Finding Second Maximum"
+02:17:17:2 Submitted "Finding Second Maximum" again, used pq instead of just one sort
+02:21:33:5 Submitting "Sticks"
+02:25:25:3 Both AC!!!

c2da9121dd1b8f3a57829c521eeec4697c27f4ab
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2018-08-05 15:41:40 +0800

    Update 18-R2-C.java

--- FacebookHackerCup/18-R2-C.java
+++ FacebookHackerCup/18-R2-C.java
@@ -4,45 +4,72 @@ import java.util.*;
 public class Main {
 	static final Reader in = new Reader();
 	static final PrintWriter out = new PrintWriter(System.out);
-	static List<Integer>[] adj;
-	static int[] d;
-	static long ans;
+	static long M=(long)1e9+7;
 	
 	public static void main(String[] args) {
 		for(int tests=in.nextInt(), ti=1; ti<=tests; ++ti) {
-			int n=in.nextInt(), m=in.nextInt();
-			long a=in.nextInt(), b=in.nextInt();
-			adj = new List[n];
+			int n=in.nextInt(), s=in.nextInt(), e=in.nextInt();
+			int[] x = new int[n], y = new int[n], ya;
+			List<Integer> p = new ArrayList<Integer>();
+			for(int i=0; i<n; ++i) {
+				x[i]=in.nextInt();
+				y[i]=in.nextInt();
+				p.add(i);
+			}
+			Collections.sort(p, new Comparator<Integer>() {
+				public int compare(Integer a, Integer b) {
+					return x[a]-x[b];
+				}
+			});
+			ya = Arrays.copyOf(y, n+2);
+			ya[n]=s;
+			ya[n+1]=e;
+			Arrays.sort(ya);
+			s=Arrays.binarySearch(ya, s);
+			e=Arrays.binarySearch(ya, e);
 			for(int i=0; i<n; ++i)
-				adj[i] = new ArrayList<Integer>();
-			for(int i=1; i<n; ++i)
-				adj[in.nextInt()].add(i);
-			d = new int[n];
-			for(int i=0; i<m; ++i)
-				++d[(int)((a*i+b)%n)];
-			ans=0;
-			dfs(0);
-			out.println("Case #"+ti+": "+ans);
+				y[i]=Arrays.binarySearch(ya, y[i]);
+			long[][][][][] dp = new long[n+1][n+2-s][s+1][n+2-e][e+1];
+			dp[0][0][s][n+1-e][0]=1;
+			for(int i=0; i<n; ++i) {
+				int pi=p.get(i);
+				for(int j1=0; j1<=n+1-s; ++j1) {
+					for(int j2=0; j2<=s; ++j2) {
+						for(int k1=0; k1<=n+1-e; ++k1) {
+							for(int k2=0; k2<=e; ++k2) {
+								if(dp[i][j1][j2][k1][k2]==0)
+									continue;
+								dp[i][j1][j2][k1][k2]%=M;
+								//Up
+								if(y[pi]>=k2)
+									dp[i+1][j1][Math.min(y[pi], j2)][k1][k2]+=dp[i][j1][j2][k1][k2];
+								//Down
+								if(y[pi]<=k1+e)
+									dp[i+1][Math.max(y[pi]-s, j1)][j2][k1][k2]+=dp[i][j1][j2][k1][k2];
+								//Left
+								if(y[pi]>s?y[pi]>j1+s:y[pi]<j2)
+									dp[i+1][j1][j2][k1][k2]+=dp[i][j1][j2][k1][k2];
+								//Right
+								if(y[pi]>e)
+									dp[i+1][j1][j2][Math.min(y[pi]-e, k1)][k2]+=dp[i][j1][j2][k1][k2];
+								else
+									dp[i+1][j1][j2][k1][Math.max(y[pi], k2)]+=dp[i][j1][j2][k1][k2];
+							}
+						}
+					}
+				}
+			}
+			long a1=1, a2=0;
+			for(int i=0; i<n; ++i)
+				a1=a1*4%M;
+			for(int j1=0; j1<=n+1-s; ++j1)
+				for(int j2=0; j2<=s; ++j2)
+					for(int k1=0; k1<=n+1-e; ++k1)
+						for(int k2=0; k2<=e; ++k2)
+							a2+=dp[n][j1][j2][k1][k2];
+			out.println("Case #"+ti+": "+(a1-a2%M+M)%M);
 		}
 		out.close();
-	}
-	
-	static PriorityQueue<Integer> dfs(int u) {
-		PriorityQueue<Integer> pq = new PriorityQueue<Integer>(Collections.reverseOrder());
-		pq.add(u);
-		for(int v : adj[u]) {
-			PriorityQueue<Integer> pq2 = dfs(v);
-			if(pq2.size()>pq.size()) {
-				for(int a : pq)
-					pq2.add(a);
-				pq=pq2;
-			} else
-				for(int a : pq2)
-					pq.add(a);
-		}
-		while(d[u]-->0&&!pq.isEmpty())
-			ans+=pq.poll();
-		return pq;
 	}
 	
 	static class Reader {

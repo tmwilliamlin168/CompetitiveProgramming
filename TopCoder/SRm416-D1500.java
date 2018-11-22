67a14801a12e6525cbcfa577d3df01c69c31af2a
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2018-11-22 16:13:51 +0800

    Create SRm416-D1500.java

---
+++ TopCoder/SRm416-D1500.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class CustomDice {
+	static final int M=(int)1e9+7;
+	int[] dp = new int[5999980];
+	
+	public int countDice(int m) {
+        if(m<4)
+            return 0;
+		Arrays.fill(dp, 30);
+		for(int i=1; i<=6; ++i)
+			for(int j=i; j<=6*m-21; ++j)
+				dp[j]=(dp[j]+dp[j-i])%M;
+		return dp[6*m-21];
+	}
+}

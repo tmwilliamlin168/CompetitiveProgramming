0da23dc3d6c904cfc7f4ce0378791ab3499f2b95
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2019-01-04 22:17:56 +0800

    Update 1096G.cpp

--- CodeForces/1096G.cpp
+++ CodeForces/1096G.cpp
@@ -16,34 +16,39 @@ ll pm(ll b, int p) {
 	return r;
 }
 
-void ntt(vector<ll> &a, bool iv) {
-	int n=a.size();
-	for(int i=0, j=0; i<n; ++i) {
-		if(i>j)
-			swap(a[i], a[j]);
-		for(int k=n/2; (j^=k)<k; k/=2);
+struct ntt {
+	ll rt[1<<20];
+	void fi(int n) {
+		rt[n/2]=1;
+		rt[n/2+1]=pm(G, (M-1)/n);
+		for(int i=n/2+2; i<n; ++i)
+			rt[i]=rt[i-1]*rt[n/2+1]%M;
+		for(int i=n/2; --i>=1; )
+			rt[i]=rt[2*i];
 	}
-	for(int st=1, id=1; 2*st<=n; st*=2, ++id) {
-		ll wn=pm(G, iv?(M-1-(M-1>>id)):(M-1>>id));
-		for(int i=0; i<n; i+=2*st) {
-			ll wi=1;
-			for(int j=i; j<i+st; ++j, wi=wi*wn%M) {
-				ll u=a[j], v=wi*a[j+st]%M;
-				a[j]=u+v;
-				if(a[j]>=M)
-					a[j]-=M;
-				a[j+st]=u-v;
-				if(a[j+st]<0)
-					a[j+st]+=M;
+	void ac(vector<ll> &a) {
+		int n=a.size();
+		for(int i=0, j=0; i<n; ++i) {
+			if(i>j)
+				swap(a[i], a[j]);
+			for(int k=n/2; (j^=k)<k; k/=2);
+		}
+		for(int st=1; 2*st<=n; st*=2) {
+			for(int i=0; i<n; i+=2*st) {
+				for(int j=i; j<i+st; ++j) {
+					ll z=rt[j-i+st]*a[j+st]%M;
+					a[j+st]=a[j]-z;
+					if(a[j+st]<0)
+						a[j+st]+=M;
+					a[j]+=z;
+					if(a[j]>=M)
+						a[j]-=M;
+				}
 			}
 		}
 	}
-	if(iv) {
-		ll in=pm(n, M-2);
-		for(int i=0; i<n; ++i)
-			a[i]=a[i]*in%M;
-	}
-}
+};
+ntt nt;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -54,11 +59,13 @@ int main() {
 	vector<ll> a(1<<20);
 	while(k--)
 		cin >> di, a[di]=1;
-	ntt(a, 0);
+	nt.fi(1<<20);
+	nt.ac(a);
+	ll in=pm(1<<20, M-2), ans=0;
 	for(int i=0; i<1<<20; ++i)
-		a[i]=pm(a[i], n/2);
-	ntt(a, 1);
-	ll ans=0;
+		a[i]=pm(a[i], n/2)*in%M;
+	reverse(a.begin()+1, a.end());
+	nt.ac(a);
 	for(int i=0; i<1<<20; ++i)
 		ans+=a[i]*a[i]%M;
 	cout << ans%M;

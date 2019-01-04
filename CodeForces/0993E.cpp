c588494789eae75aab5d181161c313521afad2f3
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2019-01-04 21:25:27 +0800

    Update 0993E.cpp

--- CodeForces/0993E.cpp
+++ CodeForces/0993E.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-
 template<typename T> struct cp {
 	T x, y;
 	cp<T> operator+(const cp &o) const {
@@ -12,33 +10,51 @@ template<typename T> struct cp {
 		return {x*o.x-y*o.y, x*o.y+y*o.x};
 	}
 };
-
-template<typename T> void fft(vector<cp<T>> &a, bool iv) {
-	int n=a.size();
-	for(int i=0, j=0; i<n; ++i) {
-		if(i>j)
-			swap(a[i], a[j]);
-		for(int k=n/2; (j^=k)<k; k/=2);
+template<typename T> struct fft {
+	static const int mxN=1<<19;
+	cp<T> rt[mxN];
+	void fi(int n) {
+		T PI=acos((T)-1);
+		for(int i=0; i<n/2; ++i) {
+			T an=2*PI*i/n;
+			rt[i+n/2]={cos(an), sin(an)};
+		}
+		for(int i=n/2; --i>=1; )
+			rt[i]=rt[2*i];
 	}
-	for(int st=1; 2*st<=n; st*=2) {
-		T an=acos((T)-1)/st*(iv?-1:1);
-		cp<T> wn={cos(an), sin(an)}, wi={1, 0};
-		for(int i=0; i<n; i+=2*st) {
-			cp<T> wi={1, 0};
-			for(int j=i; j<i+st; ++j, wi=wi*wn) {
-				cp<T> u=a[j], v=wi*a[j+st];
-				a[j]=u+v;
-				a[j+st]=u+v*cp<T>{-1, 0};
+	void ac(vector<cp<T>> &a) {
+		int n=a.size();
+		for(int i=0, j=0; i<n; ++i) {
+			if(i>j)
+				swap(a[i], a[j]);
+			for(int k=n/2; (j^=k)<k; k/=2);
+		}
+		for(int st=1; 2*st<=n; st*=2) {
+			for(int i=0; i<n; i+=2*st) {
+				for(int j=i; j<i+st; ++j) {
+					cp<T> z=rt[j-i+st]*a[j+st];
+					a[j+st]=a[j]+cp<T>{-1, 0}*z;
+					a[j]=a[j]+z;
+				}
 			}
 		}
 	}
-	if(iv)
-		for(int i=0; i<n; ++i)
-			a[i].x/=n;
-}
+	void mul(vector<cp<T>> &g, vector<cp<T>> &h) {
+		int n=g.size();
+		ac(g);
+		for(int i=0; i<n; ++i) {
+			int j=(n-i)&(n-1);
+			cp<T> g2=g[i]*g[i];
+			g2.x=-g2.x;
+			h[i]=(g[j]*g[j]+g2)*cp<T>{(T)0, (T)-1/4/n};
+		}
+		ac(h);
+	}
+};
+fft<double> f;
 
 const int mxN=2e5;
-int n, x, ps[mxN+1];
+int n, x, ps[mxN+1], ai;
 
 int main() {
 	ios::sync_with_stdio(0);
@@ -46,21 +62,17 @@ int main() {
 	
 	cin >> n >> x;
 	for(int i=0; i<n; ++i) {
-		int ai;
 		cin >> ai;
 		ps[i+1]=ps[i]+(ai<x);
 	}
 	vector<cp<double>> a(1<<19), b(1<<19);
 	for(int i=0; i<=n; ++i) {
 		++a[ps[i]].x;
-		++b[n-ps[i]].x;
+		++a[n-ps[i]].y;
 	}
-	fft(a, 0);
-	fft(b, 0);
-	for(int i=0; i<1<<19; ++i)
-		a[i]=a[i]*b[i];
-	fft(a, 1);
-	cout << ((ll)round(a[n].x)-n-1)/2;
+	f.fi(1<<19);
+	f.mul(a, b);
+	cout << ((long long)round(b[n].x)-n-1)/2;
 	for(int i=n+1; i<=2*n; ++i)
-		cout << " " << (ll)round(a[i].x);
+		cout << " " << (long long)round(b[i].x);
 }

8cc9039b07c081231d04710e64561ba233d4da2e
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2019-03-12 23:20:48 +0800

    Update 18S-Bitaro.cpp

--- JOI/18S-Bitaro.cpp
+++ JOI/18S-Bitaro.cpp
@@ -1,45 +1,39 @@
 /*
 	- http://codeforces.com/blog/entry/58433?#comment-421295
-	- For some reason, the runtime is the lowest when the block size is around 10
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#define pii pair<int, int>
-#define fi first
-#define se second
-
-const int mxN=1e5, bs=10;
+ 
+const int mxN=1e5, bs=60;
 int n, m, q, dp2[mxN], c[mxN];
 vector<int> adj[mxN];
-vector<pii> dp1[mxN];
-priority_queue<pii, vector<pii>, greater<pii>> pq;
+vector<array<int, 2>> dp1[mxN];
 bool a[mxN];
-
+ 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	
 	cin >> n >> m >> q;
-	while(m--) {
-		int u, v;
-		cin >> u >> v, --u, --v;
-		adj[v].push_back(u);
-	}
+	for(int i=0, u, v; i<m; ++i)
+		cin >> u >> v, --u, --v, adj[v].push_back(u);
 	for(int i=0; i<n; ++i) {
-		pq.push({0, i});
+		vector<int> w={i};
 		for(int v : adj[i]) {
-			for(pii p : dp1[v]) {
-				pq.push({p.fi+1, p.se});
-				if(pq.size()>bs)
-					pq.pop();
+			for(array<int, 2> p : dp1[v]) {
+				if(!c[p[1]])
+					w.push_back(p[1]);
+				c[p[1]]=max(p[0]+1, c[p[1]]);
 			}
 		}
-		while(!pq.empty()) {
-			dp1[i].push_back(pq.top());
-			pq.pop();
-		}
+		nth_element(w.begin(), w.begin()+min(bs, (int)w.size()), w.end(), [&](int a, int b) {
+			return c[a]>c[b];
+		});
+		for(int j=0; j<min(bs, (int)w.size()); ++j)
+			dp1[i].push_back({c[w[j]], w[j]});
+		for(int wi : w)
+			c[wi]=0;
 	}
 	while(q--) {
 		int t, y, ans=-1;
@@ -48,22 +42,17 @@ int main() {
 			cin >> c[i], --c[i];
 			a[c[i]]=1;
 		}
-		if(y>bs) {
-			memset(dp2, -1, 4*(t+1));
+		if(y>=bs) {
 			for(int i=0; i<=t; ++i) {
 				dp2[i]=a[i]?-n:0;
 				for(int v : adj[i])
 					dp2[i]=max(dp2[v]+1, dp2[i]);
 			}
 			ans=max(dp2[t], ans);
-		} else {
-			for(int i=dp1[t].size()-1; i>=0; --i) {
-				if(a[dp1[t][i].se])
-					continue;
-				ans=dp1[t][i].fi;
-				break;
-			}
-		}
+		} else 
+			for(array<int, 2> p : dp1[t])
+				if(!a[p[1]])
+					ans=max(p[0], ans);
 		for(int i=0; i<y; ++i)
 			a[c[i]]=0;
 		cout << ans << "\n";

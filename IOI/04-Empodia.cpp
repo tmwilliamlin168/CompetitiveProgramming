b94482db3313cfdc00bc9da2dc2a6cc97aa26a4d
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2019-06-13 20:22:01 +0800

    Create 04-Empodia.cpp

---
+++ IOI/04-Empodia.cpp
@@ -0,0 +1,46 @@
+/*
+	- Note that maximum m is changed to 2e6 on yandex
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mxN=2e6;
+int n, a[mxN], r[mxN], l[mxN], b[mxN];
+vector<int> v[2*mxN-1], ans;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for(int i=0; i<n; ++i) {
+		cin >> a[i];
+		l[i]=i-1;
+		while(~l[i]&&a[l[i]]<a[i])
+			l[i]=l[l[i]];
+		v[a[i]-i+n-1].push_back(i);
+	}
+	for(int i=n-1; ~i; --i) {
+		r[i]=i+1;
+		while(r[i]<n&&a[r[i]]>a[i])
+			r[i]=r[r[i]];
+	}
+	for(int k=0; k<2*n-1; ++k) {
+		vector<int> w;
+		for(int i : v[k]) {
+			while(w.size()&&r[w.back()]<i)
+				w.pop_back();
+			b[i]=w.size()&&l[i]<=w.back()?w.back():i;
+			while(w.size()&&r[w.back()]<r[i])
+				w.pop_back();
+			w.push_back(i);
+		}
+	}
+	for(int i=1; i<n; ++i)
+		if(b[i]<i&&(!ans.size()||b[i]>b[ans.back()]))
+			ans.push_back(i);
+	cout << ans.size() << "\n";
+	for(int i : ans)
+		cout << b[i]+1 << " " << i+1 << "\n";
+}

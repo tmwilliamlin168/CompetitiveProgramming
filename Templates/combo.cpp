26ccd8d136560d7fd986274e7416335c1b4df13b
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2020-07-11 15:59:39 +0800

    Update combo.cpp

--- Templates/combo.cpp
+++ Templates/combo.cpp
@@ -1,38 +1,28 @@
-template<int N> struct combo {
-	Mnt iv[N+1], f1[N+1], f2[N+1];
-	combo() {
-		iv[1]=1;
-		FOR(i, 2, N+1)
-			iv[i]=-M/i*iv[M%i];
-		f1[0]=f2[0]=1;
-		FOR(i, 1, N+1) {
-			f1[i]=f1[i-1]*i;
-			f2[i]=f2[i-1]*iv[i];
-		}
+vt<Mnt> iv, f1, f2;
+void comboInit(int n) {
+	vti(iv, 1, n+1);
+	vti(f1, 1, n+1);
+	vti(f2, 1, n+1);
+	FOR(i, 2, n+1)
+		iv[i]=-M/i*iv[M%i];
+	FOR(i, 1, n+1) {
+		f1[i]=f1[i-1]*i;
+		f2[i]=f2[i-1]*iv[i];
 	}
-	friend Mnt inv2(int n) {
-		return iv[n];
-	}
-	friend Mnt fact(int n) {
-		return f1[n];
-	}
-	friend Mnt invfact(int n) {
-		return f2[n];
-	}
-	friend Mnt nck(int n, int k) {
-		return f1[n]*f2[k]*f2[n-k];
-	}
-	friend Mnt nck2(int n, int k) {
-		umin(k, n-k);
-		Mnt r=f2[k];
-		FOR(k)
-			r*=n-i;
-		return r;
-	}
-	friend vt<Mnt> gp(int n, Mnt b) {
-		vt<Mnt> v(n+1, 1);
-		FOR(n)
-			v[i+1]=v[i]*b;
-		return v;
-	}
-};
+}
+Mnt nck(int n, int k) {
+	return f1[n]*f2[k]*f2[n-k];
+}
+Mnt nck2(int n, int k) {
+	umin(k, n-k);
+	Mnt r=f2[k];
+	FOR(k)
+		r*=n-i;
+	return r;
+}
+vt<Mnt> gp(int n, Mnt b) {
+	vt<Mnt> v(n+1, 1);
+	FOR(n)
+		v[i+1]=v[i]*b;
+	return v;
+}

9e5f369d30f748c1b06b093eea1afb618f957d38
tmwilliamlin168 <tmwilliamlin168@gmail.com>
2020-07-11 15:43:01 +0800

    Update combo.cpp

--- Templates/combo.cpp
+++ Templates/combo.cpp
@@ -1,5 +1,5 @@
-template<int N, int M> struct combo {
-	Mnt<M> iv[N+1], f1[N+1], f2[N+1];
+template<int N> struct combo {
+	Mnt iv[N+1], f1[N+1], f2[N+1];
 	combo() {
 		iv[1]=1;
 		FOR(i, 2, N+1)
@@ -10,27 +10,27 @@ template<int N, int M> struct combo {
 			f2[i]=f2[i-1]*iv[i];
 		}
 	}
-	Mnt<M> inv2(int n) {
+	friend Mnt inv2(int n) {
 		return iv[n];
 	}
-	Mnt<M> fact(int n) {
+	friend Mnt fact(int n) {
 		return f1[n];
 	}
-	Mnt<M> invfact(int n) {
+	friend Mnt invfact(int n) {
 		return f2[n];
 	}
-	Mnt<M> nck(int n, int k) {
+	friend Mnt nck(int n, int k) {
 		return f1[n]*f2[k]*f2[n-k];
 	}
-	Mnt<M> nck2(int n, int k) {
+	friend Mnt nck2(int n, int k) {
 		umin(k, n-k);
-		Mnt<M> r=f2[k];
+		Mnt r=f2[k];
 		FOR(k)
 			r*=n-i;
 		return r;
 	}
-	vt<Mnt<M>> gp(int n, Mnt<M> b) {
-		vt<Mnt<M>> v(n+1, 1);
+	friend vt<Mnt> gp(int n, Mnt b) {
+		vt<Mnt> v(n+1, 1);
 		FOR(n)
 			v[i+1]=v[i]*b;
 		return v;
